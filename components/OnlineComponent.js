bd0f6b417a69fdc4cdcdcf16903d349741dba262
KaiPereira <88850028+KaiPereira@users.noreply.github.com>
2022-10-26 18:01:07 -0700

    replaced a's with Links

--- components/OnlineComponent.js
+++ components/OnlineComponent.js
@@ -12,7 +12,7 @@ export default function OnlineComponent() {
                 <div className="onlinePaymentInformation">
                     <p>Payments may be by cheque payable to: Reynolds Secondary School or electronically through the Reynolds Secondary website. Tax receipts will be mailed to the donor by the school district!<br /><br /></p>
                     
-                    <strong>Please specific a "Fund Destination" of "Reynolds Secondary" and a "Message to the School Board" of "Robotics Club".</strong><br /><br />
+                    <strong>Please specific a &quot;Fund Destination&quot; of &quot;Reynolds Secondary&quot; and a &quot;Message to the School Board&quot; of &quot;Robotics Club&quot;.</strong><br /><br />
                     <strong>Email business information like name, amount donated, address, phone, email and website to </strong>
                 </div>
                 <Link href="https://sd61.schoolcashonline.com/Fee/Details/2967/140/false/true" target="_blank" rel="noreferrer">
